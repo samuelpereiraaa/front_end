25e831a0252dfa495bf92801a68da900405bf37e
samuel <samuelpereira.dev@gmail.com>
2025-05-29 18:56:54 -0300

    tamanho_e_quantidade/pagina_produto

--- 01_HTML/loja/js/script.js
+++ 01_HTML/loja/js/script.js
@@ -24,3 +24,30 @@ function trocarImagem(novaImagemSrc, miniaturaClicada) {
     }
 }
 
+// Função para adicionar quantidade e tamanho para o carrinho.
+
+function adicionarCarrinho () {
+    // peganndo os elementos quantidade e tamanho
+    const tamanhoSelecionado = document.getElementById('tamanho').value
+    const quantidadeSelecionada = document.getElementById('quantidade').value
+
+
+    // alert para mostrar na tela os elementos selecionados 
+    alert(` ADICIONADOS AO CARRINHO COM SUCESSO!
+            Tamanho: ${tamanhoSelecionado}
+            Quantidade: ${quantidadeSelecionada}`)
+
+    // mostrando no console
+    console.log(`Tamanho selecionando: ${tamanhoSelecionado}, Quantidade selecionanda: ${quantidadeSelecionada}`)
+}
+    // pegando o elemento do botao-comprar
+    const botaoComprar = document.getElementById('botao-comprar') 
+
+    //adicionando um event linster no botao, para adicionar ao carrinho
+    if (botaoComprar) {
+        botaoComprar.addEventListener('click', function(event){
+            event.preventDefault // retirar o comportamento padrao do botao
+            adicionarCarrinho() // adicionando a função ao botao
+        }
+    )
+    }
